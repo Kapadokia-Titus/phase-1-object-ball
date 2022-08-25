094091555b6b44576ec962b1944bfc3fcbc6a241
+ <+>
2022-08-25 07:20:24 +0300

    made some adjustments

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -26,3 +26,51 @@ function goodPractices() {
 
 // then, call the function so it runs!
 goodPractices()
+
+
+function numPointsScored(playerName){
+  return Object.key(playerName).number
+}
+
+function shoeSize(playerName){
+  return Object.key(playerName).shoe
+}
+
+function teamColors(teamName){
+  return teamName.colors
+}
+
+function teamNames(game){
+ // loop through the object
+  for(let item of game){
+    return Object.values(item).teamName
+  }
+ 
+}
+
+function teamNames(game){
+  let teams =[];
+  // loop through the object
+   for(let item of game){
+    let teamName =  Object.values(item).teamName
+     teams.push(teamName);
+   }
+  
+   return teams;
+ }
+
+ function playerNumbers(teamName){
+  let jerseyNumber =[];
+  // loop through the object
+   for(let item of teamName){
+    let num =  Object.values(item).number
+    jerseyNumber.push(num);
+   }
+  
+   return jerseyNumber;
+ }
+
+ function playerStats(playerName){
+ 
+   return Object.values(playerName);
+ }
